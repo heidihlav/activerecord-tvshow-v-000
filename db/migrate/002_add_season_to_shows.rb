e991c0669aa4790d7cda64e8974a0bcbf0596280
Heidi Hlavinka <hchlav@gmail.com>
2018-11-19 16:14:59 +0000

    automatically backed up by learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,7 +1,8 @@
+class AddSeasonToShows < ActiveRecord::Migration
+  def change
+    add_column :artists, :favorite_food, :string
+  end
+end
 
-
-
-
-
-# write a migration to add a column, season, to the shows table. 
+# write a migration to add a column, season, to the shows table.
 # The datatype of this column is string.
