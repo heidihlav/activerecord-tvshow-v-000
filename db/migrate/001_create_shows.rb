7834894fd92d321e342156bdfea2592d3669a907
Heidi Hlavinka <hchlav@gmail.com>
2018-11-19 15:45:11 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,17 @@
+class CreateShows < ActiveRecord::Migration[5.1]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.float :network
+      t.string :day
+      t.string :rating
+      t.timestamps
+    end
+  end
+end
 
 
-
-migrate create shows table
-name, network, day, and rating columns.
-name, network, and day have a datatype of string, and
-rating has a datatype of integer.
+# migrate create shows table
+# name, network, day, and rating columns.
+# name, network, and day have a datatype of string, and
+# rating has a datatype of integer.
